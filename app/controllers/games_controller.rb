771adbd922b5f472b9207a5fd7126c571345e033
brossetti1 <brossetti1@gmail>
2015-02-20 00:09:04 -0500

    added pick move, working for player1 only right now

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,18 @@
 class GamesController < ApplicationController
   before_action :authenticate_user_from_token!
-  before_action :set_game, only: [:show, :join]
+  before_action :set_game, only: [:show, :join, :update]
 
 
   def update
-    
+    binding.pry
+    board_before_move = @game.board.map(&:deep_dup)
+    @game.pick_move(current_user, token_start_params, token_end_params)
+    binding.pry
+    if @game.board != board_before_move
+      render json: { :game => @game }, status: :ok
+    else
+      render json: { messages: @game.move_error}, status: :not_acceptable
+    end
   end
 
   def join
@@ -25,24 +33,39 @@ class GamesController < ApplicationController
     end
   end
 
-  def challange
-    @user = User.find(params[:id])
-    @game = Game.new(users: [current_user, @user])
+  def challenge
+    binding.pry
+    @player1 = User.find_by_authentication_token(player1_challenge_params)
+    @player2 = User.find_by_authentication_token(player2_challange_params)
+    @game = Game.new(users: [@player1, @player2])
     if @game.save
       render json: {game: @game}, status: :created
     else
       render json: {messages: @game.errors.full_messages}, status: :unprocessable_entity
     end
   end
-
   private
-
-  def user_params
-    params.require(:user).permit(:email, :password, :username)
-  end
 
   def set_game
     @game = Game.find(params[:id])
+  end
+
+  def player1_challenge_params
+    params.require(:challenge).permit(:authentication_token_p1)
+  end
+
+  def player2_challange_params
+    params.require(:challenge).permit(:authentication_token_p2)
+  end
+
+  def token_start_params
+    token_start = params.require(:pick).permit(:token_start)
+    JSON.parse(token_start['token_start'])
+  end
+
+  def token_end_params
+    token_end = params.require(:pick).permit(:token_end)
+    JSON.parse(token_end['token_end'])
   end
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,22 +2,22 @@ class Game < ActiveRecord::Base
   has_many :players
   has_many :users, through: :players
 
-  after_validation :new_board!
+  #after_create :new_board!
 
   validates_length_of :users, maximum: 2, message: "you can only have two players."
 
     def as_json(opts={})
-      super(:only => [:board])
+      super(:only => [:id, :board])
     end
 
-  INITIAL_BOARD = [[0, 1, 0 ,1, 0, 1, 0, 1],
-                   [1, 0, 1, 0, 1, 0, 1, 0],
-                   [0, 1, 0, 1, 0, 1, 0, 1],
-                   [0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0],
+  INITIAL_BOARD = [[0, 2, 0 ,2, 0, 2, 0, 2],
                    [2, 0, 2, 0, 2, 0, 2, 0],
                    [0, 2, 0, 2, 0, 2, 0, 2],
-                   [2, 0, 2, 0, 2, 0, 2, 0]]
+                   [0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0],
+                   [1, 0, 1, 0, 1, 0, 1, 0],
+                   [0, 1, 0, 1, 0, 1, 0, 1],
+                   [1, 0, 1, 0, 1, 0, 1, 0]]
 
   serialize :board
 
@@ -33,5 +33,60 @@ class Game < ActiveRecord::Base
     self.update_attribute :board, INITIAL_BOARD
   end
 
+  def player1; self.users[0]; end
+  def player2; self.users[1]; end
 
+  def pick_move(user, token_start, token_end)
+    binding.pry
+    x1, y1 = token_start
+    if player1 == user && self.board[x1][y1] == 1
+      x2 = xBottomCheck(token_start, token_end)
+      y2 = yBottomCheck(token_start, token_end)
+      if check_integer(x2) && check_integer(y2) && valid_spot?(x2,y2)
+        self.board[x1][y1] = 0
+        self.board[x2][y2] = 1
+        self.save
+      else
+        move_error
+      end
+    end
+  end
+
+  def valid_spot?(x,y)
+    self.board[x][y] == 0 ? true : false 
+  end
+
+  def check_integer(index)
+    index.is_a?(Integer)
+  end
+
+  def xBottomCheck(token_start, token_end)
+    x1 = token_start[0]
+    x2 = token_end[0]
+    if x1 < x2 #
+      nil
+    elsif x2 > x1 - 1
+      nil
+    else
+      x2
+    end
+  end
+
+  def yBottomCheck(token_start, token_end)
+    x1, y1 = token_start
+    x2, y2 = token_end
+    if y1 == 0
+      x2 == (x1 - 1) && y2 == (y1 + 1) ? y2 : nil
+    elsif y1 == 7
+      x2 == (x1 - 1) && y2 == (y1 - 1) ? y2 : nil
+    elsif y2 == y1 + 1 || y2 == y1 - 1
+      y2
+    else y2 > y1 + 1 || y2 < y1 - 1
+      nil
+    end
+  end
+
+  def move_error
+    "you cant move there"
+  end
 end
